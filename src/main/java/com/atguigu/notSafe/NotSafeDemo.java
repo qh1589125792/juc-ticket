f3ca887bb7aa34bcf646ebf1508dff14845e6e63
qinhan <13849808656@163.com>
2019-10-11 21:40:58 +0800

    线程不安全 List<String> list = new ArrayList<String>(); 线程安全 List<String> list = new Vector<>(); List<String> list = Collections.synchronizedList(new ArrayList<>()); List<String> list = new CopyOnWriteArrayList<String>();

--- src/main/java/com/atguigu/notSafe/NotSafeDemo.java
+++ src/main/java/com/atguigu/notSafe/NotSafeDemo.java
@@ -1,13 +1,19 @@
 package com.atguigu.notSafe;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class NotSafeDemo {
 	public static void main(String[] args) {
 		//用ArrayList模拟高并发
-		List<String> list = new ArrayList<String>();
+		//List<String> list = new ArrayList<String>();
+		//List<String> list = new Vector<>();
+		//List<String> list = Collections.synchronizedList(new ArrayList<>());
+		List<String> list = new CopyOnWriteArrayList<String>();
 		for (int i = 0;i<=30; i++) {
 			new Thread(()->{
 				list.add(UUID.randomUUID().toString().substring(0, 6));
@@ -15,7 +21,7 @@ public class NotSafeDemo {
 				
 			}, String.valueOf(i)).start();
 		}
-		
+	
 	}
 
 }
